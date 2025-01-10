d320b0e657fd8e1c7873d7e968a7c485dc2cabb9
bruno marques <brunomdsv5@gmail.com>
2025-01-10 12:22:30 -0300

    avancei com a extracao do nome do livro de uma das categorias

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import requests
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
 
-url='https://books.toscrape.com/'
 
-response=requests.get(url)
-print(response)
+
+url='https://books.toscrape.com/catalogue/category/books/history_32/index.html'
+
+headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36'}
+
+page=requests.get(url, headers=headers)
+
+soup=BeautifulSoup(page.text, 'html.parser')
+var = soup.find_all('a')[55].get('title')
+print(var)
