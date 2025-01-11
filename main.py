011d8bea2d1fb73126a7ad334769e22545bc060e
bruno marques <brunomdsv5@gmail.com>
2025-01-10 23:31:56 -0300

    codigo para puxar todos os nomes

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
 
-
 url='https://books.toscrape.com/catalogue/category/books/history_32/index.html'
-
 headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36'}
-
 page=requests.get(url, headers=headers)
 
 soup=BeautifulSoup(page.text, 'html.parser')
 var = soup.find_all('a')[55].get('title')
-print(var)
+
+fetch_title = soup.find_all('a')
+
+fetch=[title.get('title') for i, title in enumerate(fetch_title) if i >= 55 and i % 2 != 0]
+
+for book in fetch:
+    print(f'\n{book}')
