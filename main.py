de345d4a7aa1dce544277879b5d9f7db42b365ef
bruno marques <brunomdsv5@gmail.com>
2025-01-12 19:35:20 -0300

    criado dicionario com nome, valor e nota dos livros

--- main.py
+++ main.py
@@ -8,20 +8,47 @@ headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/53
 page=requests.get(url, headers=headers)
 
 soup=BeautifulSoup(page.text, 'html.parser')
-var = soup.find_all('a')[55].get('title')
 
 fetch_title = soup.find_all('a')
+titles=[title.get('title') for i, title in enumerate(fetch_title) if i >= 55 and i % 2 != 0]
 
-fetch=[title.get('title') for i, title in enumerate(fetch_title) if i >= 55 and i % 2 != 0]
 
-precos=[]
 fetch_price = soup.find_all('p', class_='price_color')
-for i in fetch_price:
-    x = str(i.get_text()).split('Â')
-    precos.append(x[1])
-    
-print(precos)
+prices=[str(i.get_text()).split('Â£')[1] for i in fetch_price]
 
+prices = [*map(float, prices)]
+
+books = []
+
+rating = soup.find_all('p', class_='star-rating')
+rating_transformed=[*map(str, rating)]
+
+reviews = []
+
+for string in rating_transformed:
+    if 'One' in string:
+        reviews.append(1)
+
+    if 'Two' in string:
+        reviews.append(2)
+
+    if 'Three' in string:
+        reviews.append(3)
+
+    if 'Four' in string:
+        reviews.append(4)
+
+    if 'Five' in string:
+        reviews.append(5)
+    
+for title, price, review in zip(titles, prices, reviews):
+    dic={}
+    dic['name']=title
+    dic['price']=price
+    dic['rating']=review
+    books.append(dic)
+
+print(books[2])
 
 
 
